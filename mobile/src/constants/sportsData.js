2c68d0837c1d284d8e2262e9b5edc342b77fbda2
JHAJI01 <kr.gautamjha03@gmail.com>
2026-02-22 04:14:49 +0530

    feat: add sports list data to constants

---
+++ mobile/src/constants/sportsData.js
@@ -0,0 +1,7 @@
+export const sports = [
+    { name: 'Football', icon: '⚽', color: '#1a472a' },
+    { name: 'Cricket', icon: '🏏', color: '#2e4d2a' },
+    { name: 'Basketball', icon: '🏀', color: '#5c3a21' },
+    { name: 'Tennis', icon: '🎾', color: '#1e3a5f' },
+    { name: 'Badminton', icon: '🏸', color: '#4a1e5f' },
+];

520335205cd9c4b0b51b3834b3e319b1182937f3
JHAJI01 <kr.gautamjha03@gmail.com>
2026-02-22 04:15:31 +0530

    feat: add beginner gear data to constants

--- mobile/src/constants/sportsData.js
+++ mobile/src/constants/sportsData.js
@@ -5,3 +5,9 @@ export const sports = [
     { name: 'Tennis', icon: '🎾', color: '#1e3a5f' },
     { name: 'Badminton', icon: '🏸', color: '#4a1e5f' },
 ];
+
+export const beginnerGear = [
+    { id: 'bg1', name: 'Phantom GX Elite', price: '₹19,995', image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=400', brand: 'Nike' },
+    { id: 'bg2', name: 'AeroSwift Match', price: '₹5,499', image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?q=80&w=400', brand: 'Nike' },
+    { id: 'bg3', name: 'Predator Precision', price: '₹14,999', image: 'https://images.unsplash.com/photo-1539185441755-769473a23570?q=80&w=400', brand: 'Adidas' },
+];
